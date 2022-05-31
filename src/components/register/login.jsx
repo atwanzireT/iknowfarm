a723e4143ab0b7b6a7e816869a856f55029a112a
atwanzireT <tatwanzire@gmail.com>
2022-05-31 05:57:22 +0300

    Worked o the login page

---
+++ src/components/register/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RegisterCard from './registerCard';
+
+
+function Login() {
+    return (
+        <div>
+            <div className='container text-center'>
+                <RegisterCard/>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
