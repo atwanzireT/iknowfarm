dd8c5d76fc1c5a037b2bc3ff0875b08e14abdf53
atwanzireT <tatwanzire@gmail.com>
2022-06-01 22:58:27 +0300

    working on the side bar

---
+++ backend/static/js/jquery.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#crops").click(function(){
+      $("#all-crops").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#livestock").click(function(){
+      $("#all-livestock").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#farmers").click(function(){
+      $("#all-farmers").slideToggle("slow");
+    });
+  });
+
+$(document).ready(function(){
+    $("#translations").click(function(){
+      $("#all-translations").slideToggle("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#utilities").click(function(){
+      $("#district").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#upload").click(function(){
+      $("#all-uploads").slideToggle("slow");
+    });
+});
