aab8ee5f156ea8ed73692ae58f8f556753c9c079
atwanzireT <tatwanzire@gmail.com>
2022-06-07 11:36:09 +0300

    created the video upload model

--- backend/upload/models.py
+++ backend/upload/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from home.models import Crop, Livestock
 
 # Create your models here.
-def CSVupload(request):
-    return render(request, 'farmercsv.html', {})
+class VideoUpload(models.Model):
+    title = models.CharField(max_length=45)
+    english_video = models.FileField(upload_to='english_videos/')
+    arabic_video = models.FileField(upload_to='arabic_videos/')
+    lugba_video = models.FileField(upload_to='lugba_videos/')
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    crop_id = models.ForeignKey(Crop, on_delete=models.CASCADE, blank=True, null=True)
+    Livestock_id = models.ForeignKey(Livestock, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
