d8f186cc73191183dab7cf5a8cd627c43eb609c2
atwanzireT <tatwanzire@gmail.com>
2022-06-07 18:54:06 +0300

    working on video uploads

--- backend/upload/urls.py
+++ backend/upload/urls.py
@@ -1,11 +1,13 @@
 from django import views
-from .views import CSVupload, VideoUploads, AddVideoView
+from .views import CSVupload, CropVideoUploads, AddVideoView, LivestockVideoUploads, VideoUploads
 from .models import *
 from django.urls import path
 
 
 urlpatterns = [
     path('data/', CSVupload, name="uploadCsv"),
-    path('videos/', VideoUploads, name="uploadVideo"),
+    path('crop_videos/', CropVideoUploads, name="CropVideo"),
+    path('livestock_videos/', LivestockVideoUploads, name="LivestockVideo"),
+    path('videos/', VideoUploads, name="video"),
     path('addVideo/', AddVideoView.as_view(), name="addVideo"),
 ]
--- backend/upload/views.py
+++ backend/upload/views.py
@@ -3,24 +3,43 @@ from .models import VideoUpload
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from .form import AddVideoForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/profile/login/')
 def CSVupload(request):
     return render(request, 'farmercsv.html', {})
 
-def VideoUploads(request):
+@login_required(login_url='/profile/login/')
+def CropVideoUploads(request):
     crop_videos = VideoUpload.objects.filter(crop_id__isnull=False)
-    livestock_videos = VideoUpload.objects.filter(Livestock_id__isnull=False)
-    videos = VideoUpload.objects.all()
+
     dic = {
         'crop_videos': crop_videos,	
+    }
+    return render(request, "Cropvideo.html", dic)
+
+@login_required(login_url='/profile/login/')
+def LivestockVideoUploads(request):
+    livestock_videos = VideoUpload.objects.filter(Livestock_id__isnull=False)
+    dic = {
         'livestock_videos': livestock_videos,
-        'videos': videos
+    }
+    return render(request, "Livestockvideo.html", dic)
+
+@login_required(login_url='/profile/login/')
+def VideoUploads(request):
+    videos = VideoUpload.objects.all()
+    dic = {
+        'videos': videos,
     }
     return render(request, "video.html", dic)
-
+    
 class AddVideoView(LoginRequiredMixin, generic.CreateView):
-    model = VideoUploads
+    model = VideoUpload
     template_name = 'addVideo.html'
     form_class = AddVideoForm
     login_url = '/profile/login/'
+
+    def get_success_url(self):
+        return reverse('addLiveStock')
