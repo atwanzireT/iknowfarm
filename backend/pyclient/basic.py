b3d052dda733065faeb72aaec99b0dda8744a966
atwanzireT <tatwanzire@gmail.com>
2022-06-15 11:20:28 +0300

    worked on the client API

--- backend/pyclient/basic.py
+++ backend/pyclient/basic.py
@@ -1,10 +1,11 @@
 import requests
 
-endpoint = "http://httpbin.org/status/200"
+# endpoint = "http://httpbin.org/status/200"
 endpoint = "http://httpbin.org/anything"
 
 # Http Request
-get_reponse = requests.get(endpoint) # Application Programming Interference
+get_reponse = requests.get(endpoint, json = {"query":"Hello World"}) # Application Programming Interference
 
 # Http Reponse
-print(get_reponse.text)
+# print(get_reponse.text)
+print(get_reponse.status_code)
