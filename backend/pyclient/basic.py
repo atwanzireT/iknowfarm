42ee58543a1e074b47df1122aa725a1760fe9697
atwanzireT <tatwanzire@gmail.com>
2022-06-15 11:54:15 +0300

    created the django API app

--- backend/pyclient/basic.py
+++ backend/pyclient/basic.py
@@ -1,11 +1,12 @@
 import requests
 
 # endpoint = "http://httpbin.org/status/200"
-endpoint = "http://httpbin.org/anything"
+# endpoint = "http://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
 
 # Http Request
 get_reponse = requests.get(endpoint, json = {"query":"Hello World"}) # Application Programming Interference
 
 # Http Reponse
-# print(get_reponse.text)
+print(get_reponse.text)
 print(get_reponse.status_code)
