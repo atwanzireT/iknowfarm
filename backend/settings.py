f7b2bae5dc4a6de1984070a9e8ee68e00509ce0f
atwanzireT <tatwanzire@gmail.com>
2022-07-14 12:56:57 +0300

    Debug setting

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get('DEBUG')) == '1'
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['iknowfarm.herokuapp.com', '**.herokuapp.com', 'localhost', '127.0.0.1']
 
